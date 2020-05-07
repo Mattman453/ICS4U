203007b4c91ed066e597edc84ba96129961bec11
Matt <mattjcruickshank@gmail.com>
2020-05-07 09:57:25 -0400

    Added Sum method for 2D arrays

---
+++ 2D_Arrays/src/app/Main.java
@@ -0,0 +1,18 @@
+package app;
+
+public class Main {
+
+    private static double sum(double[][] array) {
+        double total = 0;
+        for (int i=0; i<array.length; i++) {
+            for (int j=0; j<array[0].length; j++) {
+                total+=array[i][j];
+            }
+        }
+        return total;
+    }
+
+    public static void main(String[] args) {
+        double array[][] = new double[10][5];
+    }
+}
