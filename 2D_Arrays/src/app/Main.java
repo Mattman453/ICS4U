473fc766febe0a2b4294718e82b23204a562bfe8
Matt <mattjcruickshank@gmail.com>
2020-05-07 10:04:50 -0400

    Added a max method for 2D arrays

--- 2D_Arrays/src/app/Main.java
+++ 2D_Arrays/src/app/Main.java
@@ -2,6 +2,18 @@ package app;
 
 public class Main {
 
+    private static double max(double[][] array) {
+        double max = array[0][0];
+        for (int i=0; i<array.length; i++) {
+            for (int j=0; j<array[0].length; j++) {
+                if (array[i][j]>max) {
+                    max = array[i][j];
+                }
+            }
+        }
+        return max;
+    }
+
     private static double sum(double[][] array) {
         double total = 0;
         for (int i=0; i<array.length; i++) {
