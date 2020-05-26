87ef6b6cd5df3d3b7426f83cddc0fce8a06fa9dd
Matt <mattjcruickshank@gmail.com>
2020-05-25 20:02:34 -0400

    Added Fibonacci function

--- recursionBasics/src/app/Main.java
+++ recursionBasics/src/app/Main.java
@@ -2,6 +2,12 @@ package app;
 
 public class Main {
 
+    public static int fibonacci(int n) {
+        if (n<=1)
+            return n;
+        return fibonacci(n-1)+fibonacci(n-2);
+    }
+
     public static int factorial(int n) {
         if (n>1)
             return n*factorial(n-1);
@@ -10,5 +16,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println(factorial(5));
+        System.out.println(fibonacci(6));
     }
 }
